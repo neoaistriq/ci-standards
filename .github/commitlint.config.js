8609e89b19a9f84f93833d79baa4c107df2f2609
Ranjan <Ranjan@neoaistriq.com>
2026-01-02 21:46:19 +0530

    Update commitlint.config.js

--- .github/commitlint.config.js
+++ .github/commitlint.config.js
@@ -28,7 +28,7 @@ module.exports = {
 
     // ---- Subject rules ----
     'subject-empty': [2, 'never'],
-    'subject-full-stop': [2, 'never', '.'],
+    //'subject-full-stop': [2, 'never', '.'],
     'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case']],
 
     // ---- Header rules ----
